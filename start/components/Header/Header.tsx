2afa5ae028aed2c190888fe04e0cd2a2a75ffedc
taylorjrsmith <42411334+taylorjrsmith@users.noreply.github.com>
2025-01-31 11:25:33 +0000

    commit inital

--- start/components/Header/Header.tsx
+++ start/components/Header/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
       <BackgroundAppBar position="static" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Toolbar variant="dense">
           <Typography variant="h1" component="div" sx={{ flexGrow: 1, color: 'white', fontFamily: 'Henny Penny' }}>
-            Funko Showcase
+            Taylor's Funko showcase
           </Typography>
         </Toolbar>
       </BackgroundAppBar>
